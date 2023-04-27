c5a209c3566882dac046b8676cc6bf504233dc65
Filipe Bacof <filipebacof@gmail.com>
2023-04-27 18:34:52 -0300

    registro de usuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,13 +6,42 @@ const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET;
 
 // Generate user token
-const generateToken = () => {
+const generateToken = (id) => {
     return jwt.sign({ id }, jwtSecret, { expiresIn: "7d" });
 };
 
 // Register user and sign in
 const register = async (req, res) => {
-    res.send("Registro");
+    const { name, email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (user) {
+        res.status(422).json({errors: ["Este e-mail já está em uso"]});
+        return;
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    });
+
+    // If user was created succesfully, return the token
+    if(!newUser) {
+        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]});
+        return;
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    });
 };
 
 module.exports = {
