3160fb4d37c5e98ecc30e4c57b41a414d67645ad
Filipe Bacof <filipebacof@gmail.com>
2023-04-29 19:11:21 -0300

    adicionar comentarios + bug likes

--- controllers/PhotoController.js
+++ controllers/PhotoController.js
@@ -160,6 +160,8 @@ const likePhoto = async (req, res) => {
         // Put the user ID in likes array
         photo.likes.push(reqUser._id);
 
+        await photo.save();
+
         res.status(200).json({
             photoId: id,
             userId: reqUser._id,
@@ -172,6 +174,50 @@ const likePhoto = async (req, res) => {
     }
 }
 
+// Comment functionality
+const commentPhoto = async (req, res) => {
+    const { id } = req.params;
+    const { comment } = req.body;
+    const reqUser = req.user;
+
+    try {
+        const user = await User.findById(reqUser._id);
+        try {
+            const photo = await Photo.findById(id);
+
+            // Check if photo exists
+            if (!photo) {
+                res.status(404).json({ errors: ["Foto não encontrada!"] });
+                return;
+            };
+
+            // Put comment in the array comments
+            const userComment = {
+                comment,
+                userName: user.name,
+                userImage: user.profileImage,
+                userId: user._id
+            };
+
+            photo.comments.push(userComment);
+
+            await photo.save();
+
+            res.status(200).json({
+                comment: userComment,
+                message: "O comentário foi adicionado com sucesso."
+            });
+        } catch (error) {
+            res.status(404).json({ errors: ["Foto não encontrada!"] });
+        return;
+        };
+    } catch (error) {
+        res.status(404).json({ errors: ["Usuário não encontrado!"] });
+        return;
+    };
+};
+
+
 module.exports = {
     insertPhoto,
     deletePhoto,
@@ -180,4 +226,5 @@ module.exports = {
     getPhotoById,
     updatePhoto,
     likePhoto,
+    commentPhoto,
 };
